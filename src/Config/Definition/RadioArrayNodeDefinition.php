1ce245b0bbff28d7bc6eeb7e4d977e86120b3ff5
Andy Truong <thehongtt@gmail.com>
2014-08-27 08:57:49 +0700

    Add RadioArrayDefinition.

---
+++ src/Config/Definition/RadioArrayNodeDefinition.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\atapi\Config\Definition;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+class RadioArrayNodeDefinition extends ArrayNodeDefinition
+{
+
+    private $radioOptions = [];
+
+    public function mustFillOnlyOneOfTheseKeys(array $keys)
+    {
+        $this->radioOptions[] = $keys;
+    }
+
+    protected function createNode()
+    {
+        $node = parent::createNode();
+        $node->setFinalValidationClosures([
+            [$this, 'validateRadioOptions']
+        ]);
+        return $node;
+    }
+
+    public function validateRadioOptions($value)
+    {
+        foreach ($this->radioOptions as $radioOptions) {
+            $count = 0;
+            foreach ($radioOptions as $radioKey) {
+                if (!is_null($value[$radioKey])) {
+                    $count++;
+                }
+            }
+
+            switch ($count) {
+                case 1:
+                    return $value;
+
+                case 0:
+                    throw new InvalidConfigurationException('One of these keys is required but none given: ' . implode(', ', $radioOptions));
+
+                default:
+                    throw new InvalidConfigurationException('Only one of these keys is allowed: ', implode(', ', $radioOptions));
+            }
+        }
+    }
+
+}
--- src/Config/RouteConfiguration.php
+++ src/Config/RouteConfiguration.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\atapi\Config;
 
+use Drupal\atapi\Config\Definition\RadioArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -15,9 +16,12 @@ class RouteConfiguration implements ConfigurationInterface
         $rootNode = $treeBuilder->root('atapi');
 
         /* @var $route_item NodeBuilder */
-        $route_item = $rootNode->children()->arrayNode('routes')->prototype('array')->children();
+        $route_item = $rootNode->children()
+            ->setNodeClass('radioArray', 'Drupal\atapi\Config\Definition\RadioArrayNodeDefinition')
+            ->arrayNode('routes')
+            ->prototype('radioArray');
 
-        $this->oldStyleRouteItem($route_item);
+        $this->oldStyleRouteItem($route_item->children());
         $this->templateRouteItem($route_item);
 
         return $treeBuilder;
@@ -30,22 +34,24 @@ class RouteConfiguration implements ConfigurationInterface
         $route_item->arrayNode('access arguments')->prototype('scalar');
     }
 
-    private function templateRouteItem(NodeBuilder $route_item)
+    private function templateRouteItem(RadioArrayNodeDefinition $route_item)
     {
+        $children = $route_item->children();
+
         /* @var $controller NodeBuilder */
-        $controller = $route_item->arrayNode('controller')->children();
+        $controller = $children->arrayNode('controller')->children();
         $controller->scalarNode(0)->isRequired();
         $controller->scalarNode(1)->isRequired();
         $controller->variableNode(2);
 
         // Template/Templates/Inline template
-        // @todo: validate -> developer can only provide config for one.
-        $route_item->scalarNode('content');
-        $route_item->scalarNode('template');
-        $route_item->arrayNode('templates')->prototype('scalar');
+        $route_item->mustFillOnlyOneOfTheseKeys(['content', 'template', 'templates']);
+        $children->scalarNode('content');
+        $children->scalarNode('template');
+        $children->arrayNode('templates')->prototype('scalar');
 
         // Template variables
-        $route_item->variableNode('variables');
+        $children->variableNode('variables');
     }
 
 }
